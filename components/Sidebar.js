fa78a1a4ec16f447e51880569dab1b95b4bb4e04
Dalfour <yjulius.mail@gmail.com>
2023-05-30 22:33:34 +1000

    working on sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,25 @@
+import { HomeIcon } from "@heroicons/react/outline"
+
 export default function Sidebar() {
     return (
-        <>
-        <h1>
-            Sidebar
-        </h1>
-        </>
+        <div className="hidden sm:flex flex-col">
+            <div>Logo</div>
+            <nav>
+                <SidebarLink Icon={HomeIcon} text={"Home"}/>
+                {/* <SidebarLink text={"Explore"}/>
+                <SidebarLink text={"Notifications"}/>
+                <SidebarLink text={"Messages"}/> */}
+            </nav>
+            <div>User</div>
+        </div>
     );
+}
+
+function SidebarLink({text, Icon}){
+    return (
+        <li className="flex items-center text-xl">
+            <Icon className="h-7"/>
+            {text}
+        </li>
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <div className='bg-black min-h-screen text-[#E7E9EA] max-w-[1400px] mx-auto'>
       <Sidebar />
       {/* <PostsFeed/>
       <Trending/> */}
-    </>
-  )
+    </div>
+  );
 }
