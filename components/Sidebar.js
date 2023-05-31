917059cdd5521e0f428ce8092372b22649ccd944
Dalfour <yjulius.mail@gmail.com>
2023-05-31 10:09:36 +1000

    adding sidebar links

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,14 +1,26 @@
-import { HomeIcon } from "@heroicons/react/outline"
+import { HomeIcon,
+    HashtagIcon,
+    InboxIcon,
+    BookOpenIcon,
+    ClipboardIcon,
+    BellIcon,
+    UserIcon,
+    DotsCircleHorizontalIcon,
+    BookmarkIcon,
+ } from "@heroicons/react/outline"
 
 export default function Sidebar() {
     return (
-        <div className="hidden sm:flex flex-col">
+        <div className="hidden sm:flex flex-col fixed">
             <div>Logo</div>
             <nav>
                 <SidebarLink Icon={HomeIcon} text={"Home"}/>
-                {/* <SidebarLink text={"Explore"}/>
-                <SidebarLink text={"Notifications"}/>
-                <SidebarLink text={"Messages"}/> */}
+                <SidebarLink Icon={HashtagIcon} text={"Explore"}/>
+                <SidebarLink Icon={BellIcon} text={"Notifications"}/>
+                <SidebarLink Icon={InboxIcon} text={"Messages"}/>
+                <SidebarLink Icon={BookmarkIcon} text={"Bookmarks"}/>
+                <SidebarLink Icon={UserIcon} text={"Profile"}/>
+                <SidebarLink Icon={DotsCircleHorizontalIcon} text={"More"}/>
             </nav>
             <div>User</div>
         </div>
@@ -17,9 +29,9 @@ export default function Sidebar() {
 
 function SidebarLink({text, Icon}){
     return (
-        <li className="flex items-center text-xl">
+        <li className="flex mb-6 items-center text-xl space-x-3">
             <Icon className="h-7"/>
-            {text}
+            <span className="hidden xl:inline">{text}</span>
         </li>
     )
 }
