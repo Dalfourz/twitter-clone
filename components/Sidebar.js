8e52db528fdb566c41588d5cb02da689b8b7a3fe
Dalfour <yjulius.mail@gmail.com>
2023-06-01 21:55:27 +1000

    adding sidebars components

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,37 +1,48 @@
-import { HomeIcon,
-    HashtagIcon,
-    InboxIcon,
-    BookOpenIcon,
-    ClipboardIcon,
-    BellIcon,
-    UserIcon,
-    DotsCircleHorizontalIcon,
-    BookmarkIcon,
- } from "@heroicons/react/outline"
+import {
+  HomeIcon,
+  HashtagIcon,
+  InboxIcon,
+  BookOpenIcon,
+  ClipboardIcon,
+  BellIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+  BookmarkIcon,
+} from "@heroicons/react/outline";
+import Image from "next/image";
 
 export default function Sidebar() {
-    return (
-        <div className="hidden sm:flex flex-col fixed">
-            <div>Logo</div>
-            <nav>
-                <SidebarLink Icon={HomeIcon} text={"Home"}/>
-                <SidebarLink Icon={HashtagIcon} text={"Explore"}/>
-                <SidebarLink Icon={BellIcon} text={"Notifications"}/>
-                <SidebarLink Icon={InboxIcon} text={"Messages"}/>
-                <SidebarLink Icon={BookmarkIcon} text={"Bookmarks"}/>
-                <SidebarLink Icon={UserIcon} text={"Profile"}/>
-                <SidebarLink Icon={DotsCircleHorizontalIcon} text={"More"}/>
-            </nav>
-            <div>User</div>
+  return (
+    <div className="h-full hidden sm:flex flex-col fixed xl:ml-24">
+      <nav className="h-full relative xl:space-y-1.5">
+        <div className="flex justify-center items-center xl:justify-start py-3 xl:p-3">
+          <Image
+            src={"/public/assets/twitter-logo.png"}
+            width={34}
+            height={34}
+          />
         </div>
-    );
+        <SidebarLink Icon={HomeIcon} text={"Home"} />
+        <SidebarLink Icon={HashtagIcon} text={"Explore"} />
+        <SidebarLink Icon={BellIcon} text={"Notifications"} />
+        <SidebarLink Icon={InboxIcon} text={"Messages"} />
+        <SidebarLink Icon={BookmarkIcon} text={"Bookmarks"} />
+        <SidebarLink Icon={UserIcon} text={"Profile"} />
+        <SidebarLink Icon={DotsCircleHorizontalIcon} text={"More"} />
+        <button className="hidden xl:inline bg-[#1d9bf0] rounded-full mt-2 h-[52px] w-[200px] text-lg font-bold">
+            Tweet
+        </button>
+        <div className="absolute bottom-0">User</div>
+      </nav>
+    </div>
+  );
 }
 
-function SidebarLink({text, Icon}){
-    return (
-        <li className="flex mb-6 items-center text-xl space-x-3">
-            <Icon className="h-7"/>
-            <span className="hidden xl:inline">{text}</span>
-        </li>
-    )
+function SidebarLink({ text, Icon }) {
+  return (
+    <li className="hoverAnimation flex mb-3 xl:justify-start justify-center items-center text-xl space-x-3">
+      <Icon className="h-7" />
+      <span className="hidden xl:inline">{text}</span>
+    </li>
+  );
 }
